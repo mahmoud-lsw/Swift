44ff73f580d346337f53978ebe87c0923b6bde05
Miguel Nievas <miguelnievas@ucm.es>
2018-03-03 11:21:33 +0000

    Add download_script and macro_pwl.xcm

---
+++ download_script.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+# Download data for sequences:  38104011 38104012 38104013 38104014 38104015 38104016 38104017 38104018 38104019 41539023 41539024 41539025 from obs
+# size estimates (kB):
+#   auxil: 8982069
+#     bat: 84195090
+#     xrt: 28510809
+#    uvot: 629647703
+#   TOTAL: 751335671
+# Either save this script, make it executable and run it or
+# copy the following lines into a terminal.
+# files will be downloaded to obs under your current directory
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00038104011/auxil/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00038104011/bat/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00038104011/uvot/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00038104011/xrt/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00038104012/auxil/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00038104012/bat/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00038104012/uvot/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00038104012/xrt/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00038104013/auxil/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00038104013/bat/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00038104013/uvot/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00038104013/xrt/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00038104014/auxil/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00038104014/bat/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00038104014/uvot/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00038104014/xrt/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00038104015/auxil/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00038104015/bat/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00038104015/uvot/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00038104015/xrt/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00038104016/auxil/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00038104016/bat/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00038104016/uvot/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00038104016/xrt/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00038104017/auxil/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00038104017/bat/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00038104017/uvot/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00038104017/xrt/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00038104018/auxil/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00038104018/bat/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00038104018/uvot/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00038104018/xrt/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00038104019/auxil/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00038104019/bat/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00038104019/uvot/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00038104019/xrt/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00041539023/auxil/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00041539023/bat/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00041539023/uvot/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00041539023/xrt/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00041539024/auxil/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00041539024/bat/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00041539024/uvot/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00041539024/xrt/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00041539025/auxil/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00041539025/bat/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00041539025/uvot/
+wget -nv -w 2 -nH --cut-dirs=1 -r --no-parent --reject "index.html*" http://www.swift.ac.uk/archive/reproc/00041539025/xrt/
+
